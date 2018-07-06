7a0acf53efe3615bc4e59652e2526b34c0d3926f
Hans Jung <yhansjung@gmail.com>
2018-07-06 17:18:11 -0400

    180706

--- YH_causal_estimate.R
+++ YH_causal_estimate.R
@@ -1,20 +1,15 @@
+# REFERENCE 
+## https://stackoverflow.com/questions/23852505/how-to-get-confidence-interval-for-smooth-spline
+## http://www.stat.cmu.edu/~cshalizi/402/lectures/11-splines/lecture-11.pdf
+
 # Step 0. Install necessary packages. 
-## If you already installed all, then skip the Step 0. 
-
-# install.packages('car')
-# install.packages('MASS')
-# install.packages('SuperLearner')
-# install.packages(c("caret", "glmnet", "randomForest", "RhpcBLASctl", "xgboost", "gam"))
-
 library(ggplot2) 
 library(SuperLearner)
 require(stats); require(graphics)
 library(splines)
+library(cowplot)
 
-# Step 1. Recall the data. Please refer the comment on data_generation.R 
-
-## This generate 'data' (observations)
-### 'data' is observational (Y,X,Z)
+# Seperating data frame into Y,X,Z,XZ
 sepData = function(DF){
   Yobs = DF[,1] # First column of 'data' is Y. 
   X = DF[,2] # X variable in 'data'.
@@ -23,35 +18,39 @@ sepData = function(DF){
   return(list(Yobs,X,Z,XZ))
 }
 
+# Training SuperLearner
 trainSL = function(Y,X,family, numFold){
   if (family == 'cont'){
     train_model = SuperLearner(Y = Y, X = X, family = gaussian(), cvControl = list(V=numFold),
-                               SL.library = c("SL.xgboost","SL.lm","SL.randomForest"))  
+                               SL.library = c("SL.lm","SL.randomForest", "SL.gam","SL.xgboost"))  
   }
   else if (family == 'disc'){
     train_model = SuperLearner(Y = Y, X = X, family = binomial(), cvControl = list(V=numFold),
-                               SL.library = c("SL.xgboost","SL.lm","SL.randomForest"))  
+                               SL.library = c("SL.lm","SL.randomForest","SL.gam","SL.xgboost"))  
   }
   return(train_model)
 }
 
+# Predict using the trained SL model given XZ 
 sampleEst = function(trainModel, XZ){
   predResult = predict(trainModel, XZ, onlySL=T)$pred
   return(predResult)
 }
 
+# Random sample from X (for reducing the size of X)
 reducedX = function(X, cutN = 100){
   cutN = min(N/10, cutN)
   sample_X = sample(X,cutN)
   return(sample_X)
 }
 
-sampleIntvEst = function(trainModel, sample_X, Z, cutN = 100){
+# Generate Y|do(X), where X are randomly sampled. 
+sampleIntvEst = function(trainModel, sample_X, Z, cutN = 100, N){
   Yhat_intv = rep(0, cutN)
   for (idx in 1:cutN){
     xi = sample_X[idx]
-    Zi = Z[sample(N,cutN),]
-    xi_mat = matrix(rep(xi,cutN),nrow=cutN) # N length of 0 vector
+    Zi = Z
+    xi_mat = matrix(rep(xi,N),nrow=N) # N length of xi vector
     XZi = cbind(xi_mat, Zi)
     colnames(XZi)[1] = 'X'
     Yhat_intv[idx] = mean(sampleEst(g_xz, XZi))
@@ -59,28 +58,30 @@ sampleIntvEst = function(trainModel, sample_X, Z, cutN = 100){
   return(Yhat_intv)
 }
 
+# Generate Y|x
 sampleObsEst = function(trainModel, sample_X, cutN = 100){
-  sample_X_input = data.frame(sample_X)
   sample_X_input = data.frame(sample_X)
   colnames(sample_X_input)[1] = 'X'
   Yhat_obs = sampleEst(g_x,sample_X_input)
   return(Yhat_obs)
-  
 }
 
 
+###### Spline and plotting functinos ########
 resampler <- function(data) {
   n <- nrow(data)
   resample.rows <- sample(1:n,size=n,replace=TRUE)
   return(data[resample.rows,])
 }
 
+# Estimating spline 
 spline_estimator <- function(data,n) {
   fit = smooth.spline(x=data[,1],y=data[,2],cv=TRUE)
   eval_grid = seq(from=min(data[,1]),to=max(data[,1]),length.out=n)
   return(predict(fit,x=eval_grid)$y) # We only want the predicted values
 }
 
+# Confidence interval of spline 
 spline_CI <- function(data,n,B,alpha) {
   spline_main = spline_estimator(data,n)
   spline_boots <- replicate(B,spline_estimator(resampler(data),n))
@@ -90,20 +91,26 @@ spline_CI <- function(data,n,B,alpha) {
               x=seq(from=min(data[,1]),to=max(data[,1]),length.out=n)))
 }
 
-plotResult = function(X,Y, N, B, alpha, title){
+
+
+indivPlotResult = function(X,Y,quant_X, N, B, alpha, mycolor){
   set.seed(1)
   df1 = data.frame(X, Y)
   result_spline = spline_CI(df1,N, B=B,alpha=alpha)
   df2 = data.frame( x=result_spline$x, y=result_spline$main.curve   )
   df3 = data.frame( x=result_spline$x, ymin=result_spline$lower.ci, ymax=result_spline$upper.ci   )
   
-  gg1 = ggplot(df1, aes(X, Y)) + geom_point(size=1.5)
-  gg1 = gg1 + geom_line(data=df2,aes(x=x, y=y))
-  gg1 = gg1 + geom_ribbon(data=df3, aes(x=x, ymin=ymin,ymax=ymax),alpha=0.4)
-  
+  gg1 = ggplot(df1, aes(X, Y)) + geom_point(size=1.5, color = mycolor)
+  gg1 = gg1 + geom_line(data=df2,aes(x=x, y=y), color=mycolor)
+  gg1 = gg1 + geom_ribbon(data=df3, aes(x=x, ymin=ymin,ymax=ymax),alpha=0.4, fill=mycolor)
+  gg1 = gg1 + geom_vline(data=df1,xintercept=quant_X, linetype='dotted')
+  return(gg1)
+}
+
+applyTheme = function(gg1, title){
   gg1 = gg1 + ggtitle(title)
   gg1 = gg1 + theme_bw()
-  gg1 = gg1 + scale_x_continuous(name = "X=x") + scale_y_continuous(name = "Estimated E[Y|do(x)]",limits=c(min(Y), A=max(Y)))
+  gg1 = gg1 + scale_x_continuous(name = "X") + scale_y_continuous(name = "Estimated Y",limits=c(min(Y), A=max(Y)))
   gg1 = gg1 + theme(axis.line.x = element_line(size = 0.5, colour = "black"),
                     axis.line.y = element_line(size = 0.5, colour = "black"),
                     axis.line = element_line(size=1, colour = "black"),
@@ -114,50 +121,122 @@ plotResult = function(X,Y, N, B, alpha, title){
   return(gg1)
 }
 
-################## MAIN ##################
+drawHistogram = function(X,Y){
+  df1 = data.frame(X,Y)
+  hist_X = ggplot(df1, aes(x=X)) + geom_histogram(binwidth = 0.1, fill='#FF9999', colour='black') + scale_y_reverse() + theme(axis.line.x = element_line(size = 0.5, colour = "black"),
+                                                                                                                              axis.line.y = element_line(size = 0.5, colour = "black"),
+                                                                                                                              axis.title.x= element_blank(),
+                                                                                                                              axis.line = element_line(size=1, colour = "black"),
+                                                                                                                              panel.border = element_blank(),
+                                                                                                                              panel.background = element_blank(),
+                                                                                                                              plot.title=element_text(size = 20)
+  )
+  hist_X = hist_X + scale_x_continuous(position='top')
+  return(hist_X)
+}
 
+mergePlot = function(gg1,gg2,vert_hori){
+  grid.newpage()
+  if (vert_hori == "vert"){
+    gg = plot_grid(gg1, gg2, align = "v", nrow =2, rel_heights = c(3/4, 1/4),axis='tb')
+  }
+  else if (vert_hori == "hori"){
+    gg = plot_grid(gg1, gg2, align = "v", nrow =1, rel_heights = c(1/2, 1/2),axis='tb')
+  }
+  return(gg)
+}
+
+
+
+################## MAIN ##################
+# Data generation 
 source('datagen/data_generation_cano1_cont.R') 
 
 result_sep = sepData(data_obs)
-Yobs = result_sep[[1]]
+Yobs = result_sep[[1]] 
 X = result_sep[[2]]
 Z = result_sep[[3]]
-XZ = result_sep[[4]]
+XZ = result_sep[[4]] # [X,Z]
 N = length(Yobs)
 
-numFold = 3
-n = 50
+numFold = 5
+n = 50 # number of sampling from X
 
 g_xz = trainSL(c(Yobs),XZ,'cont',numFold) # E[Y|x,z]
 g_x = trainSL(c(Yobs),data.frame(X),'cont',numFold)
 
 sample_X = reducedX(X,cutN = n)
-Yhat_intv = sampleIntvEst(g_xz, sample_X, Z, cutN = n)
+sample_X_intv = reducedX(X_intv,cutN=n)
+quant_X = quantile(X,probs=c(0,0.25,0.5,0.75,1))
+Yhat_intv = sampleIntvEst(g_xz, sample_X, Z, n, N)
 Yhat_obs = sampleObsEst(g_x, sample_X, cutN = n)
 
-B = 300
-alpha = 0.01
+B = 100 # number of bootstraping iteration for generating confidence interval. 
+alpha = 0.01 # Confidence interval (100-alpha)%
+color_intv = "red"
+color_obs = "blue"
 
-gg_intv = plotResult(sample_X,Yhat_intv,n,B,alpha,"Interventional")
-gg_obs = plotResult(sample_X, Yhat_obs,n,B,alpha,"Observational")
-library("gridExtra")
-grid.arrange(arrangeGrob(gg_intv,gg_obs,nrow=1))
+# Individual 
+gg_intv = indivPlotResult(sample_X,Yhat_intv,quant_X,n,B,alpha, color_intv)
+gg_intv = applyTheme(gg_intv,"Interventional")
+gg_obs = plotResult(sample_X, Yhat_obs,quant_X, n,B,alpha,color_obs)
+gg_obs= applyTheme(gg_obs, "Observational")
+histX = drawHistogram(X,Y)
+
+gg_merged1 = mergePlot(gg_intv,histX,'vert')
+gg_merged2 = mergePlot(gg_obs,histX,'vert')
+gg_merged = mergePlot(gg_merged1,gg_merged2,'hori')
+
+# Merged into one plot 
+gg_intv = indivPlotResult(sample_X,Yhat_intv,quant_X,n,B,alpha, color_intv)
+gg_obs = plotResult(sample_X, Yhat_obs,quant_X, n,B,alpha,color_obs)
+gg_merged = gg_intv + gg_obs
 
 # 
+# # library("gridExtra")
+# # grid.arrange(arrangeGrob(gg_intv,gg_obs,nrow=1))
+# 
+# library(grid)
 # 
 # 
-# df1 = data.frame(sample_X, Yhat_obs)
-# result_spline = spline_CI(df1, n, B=B,alpha=alpha)
-# df2 = data.frame( x=result_spline$x, y=result_spline$main.curve   )
-# df3 = data.frame( x=result_spline$x, ymin=result_spline$lower.ci, ymax=result_spline$upper.ci   )
 # 
-# gg1 = ggplot(df1, aes(sample_X, Yhat_obs)) + geom_point(size=1.5)
-# gg1 = gg1 + geom_line(data=df2,aes(x=x, y=y))
-# gg1 = gg1 + geom_ribbon(data=df3, aes(x=x, ymin=ymin,ymax=ymax),alpha=0.4)
+# plot1 = rbind(ggplotGrob(gg_intv), ggplotGrob(hist_X), size = "last")
+# plot2 = rbind(ggplotGrob(gg_obs), ggplotGrob(hist_X), size = "last")
+# grid.newpage()
+# grid.draw(cbind(plot1,plot2,size='last'))
 # 
-# gg1 = gg1 + ggtitle(title)
+# 
+# df1_intv = data.frame(x=sample_X, y=Yhat_intv)
+# result_spline_intv = spline_CI(df1_intv, n, B=B,alpha=alpha)
+# df2_intv = data.frame( x=result_spline_intv$x, y=result_spline_intv$main.curve)
+# df3_intv = data.frame( x=result_spline_intv$x, y=result_spline_intv$main.curve, ymin=result_spline_intv$lower.ci, ymax=result_spline_intv$upper.ci   )
+# 
+# gg1_intv = ggplot(df1_intv, aes(x=x, y=y)) + geom_point(size=1.5)
+# gg1_intv = gg1_intv + geom_line(data=df2_intv,aes(x=x, y=y)) 
+# gg1_intv = gg1_intv + geom_ribbon(data=df3_intv, aes(x=x, ymin=ymin, ymax=ymax),alpha=0.4)
+# 
+# df1_obs = data.frame(x=sample_X, y=Yhat_obs)
+# result_spline_obs = spline_CI(df1_obs,N, B=B,alpha=alpha)
+# df2_obs = data.frame( x=result_spline_obs$x, y=result_spline_obs$main.curve)
+# df3_obs = data.frame( x=result_spline_obs$x, y=result_spline_obs$main.curve, ymin=result_spline_obs$lower.ci, ymax=result_spline_obs$upper.ci   )
+# 
+# gg1_obs = ggplot(df1_obs, aes(x=x, y=y)) + geom_point(size=1.5)
+# gg1_obs = gg1_obs + geom_line(data=df2_obs,aes(x=x, y=y))
+# gg1_obs = gg1_obs + geom_ribbon(data=df3_obs, aes(x=x, ymin=ymin, ymax=ymax),alpha=0.4)
+# 
+# gg1_intv
+# gg1_obs
+# 
+# gg1 = ggplot()
+# gg1 = gg1 + geom_point(data=df1_intv, aes(x=x,y=y), size=1.5,color='red')
+# gg1 = gg1 + geom_point(data=df1_obs, aes(x=x,y=y), size=1.5, color='#56B4E9')
+# gg1 = gg1 + geom_line(data=df2_intv,aes(x=x, y=y),color='red') 
+# gg1 = gg1 + geom_line(data=df2_obs,aes(x=x, y=y), color='#56B4E9') 
+# gg1 = gg1 + geom_ribbon(data=df3_intv, aes(x=x, ymin=ymin, ymax=ymax),alpha=0.4,fill='red')
+# gg1 = gg1 + geom_ribbon(data=df3_obs, aes(x=x, ymin=ymin, ymax=ymax),alpha=0.4, fill='#56B4E9')
+# 
 # gg1 = gg1 + theme_bw()
-# gg1 = gg1 + scale_x_continuous(name = "X=x") + scale_y_continuous(name = "Estimated E[Y|do(x)]",limits=c(min(Y), A=max(Y)))
+# gg1 = gg1 + scale_x_continuous(name = "X=x") + scale_y_continuous(name = "Estimated Y",limits=c(min(Y), A=max(Y)))
 # gg1 = gg1 + theme(axis.line.x = element_line(size = 0.5, colour = "black"),
 #                   axis.line.y = element_line(size = 0.5, colour = "black"),
 #                   axis.line = element_line(size=1, colour = "black"),
@@ -165,9 +244,20 @@ grid.arrange(arrangeGrob(gg_intv,gg_obs,nrow=1))
 #                   panel.background = element_blank(),
 #                   plot.title=element_text(size = 20),
 #                   text=element_text(size = 16))
-# PLOT 
-## Y_intv 
 
+
+
+
+
+# 
+# library(gridExtra)
+# grid.arrange(gg1, hist_X,nrow=2)
+# grid.draw(rbind(ggplotGrob(gg1), ggplotGrob(hist_X), size = "last"))
+
+
+# gg1 = gg1 + geom_vline(data=df1,xintercept=quant_X, linetype='dotted')
+
+# plot(data_intv[,2],data_intv[,1])
 
 
 
--- datagen/data_generation_cano1_cont.R
+++ datagen/data_generation_cano1_cont.R
@@ -11,24 +11,26 @@ set.seed(1)
 
 ## Configuration for data generation 
 N = 2000 # Number of data points 
-D = 5 # Dimension of the confounding variable Z 
+D = 2 # Dimension of the confounding variable Z 
 
 # Step 1. Generate Z 
 ## In here, Z is D-dimensional multivariate normal random variable. 
 fZ = function(D,N){
   mu = 1*rnorm(D) # Set mu random. 
-  sigma = replicate(D, abs(rnorm(D))) + 10*diag(D) # Set sigma random. 
+  sigma = replicate(D, abs(rnorm(D))) + 2*diag(D) # Set sigma random. 
   Z = mvrnorm(N, mu, sigma) # Generate N numbers of D-dimensional multivariate normal random variable.
+  return(Z)
 }
 
 # Step 2. Generate X 
 ## In here, X such that Z -> X. 
 ## Specifically, X = round( exp(Bx * Z) / exp(Bx * Z) + 1 )
 fX = function(D,N,Z){
-  Bx = -1*(rnorm(D) + rgamma(D,1,1)) # Coefficient of Z. 
-  X = 0.5*matrix(Z,ncol=D) %*% matrix(Bx,ncol=1) - 1*rnorm(N)
+  Bx = -1*(abs(rnorm(D) + rgamma(D,1,1))) # Coefficient of Z. 
+  X = +0.5*matrix(Z,ncol=D) %*% matrix(Bx,ncol=1) - 0.1
   X = exp(X)/(exp(X)+1) # Note X is continuous ranged from [0,1]
   X = round(X,5)
+  return(X)
 }
 
 
@@ -36,10 +38,11 @@ fX = function(D,N,Z){
 ## Observational Y such that (X,Z) -> Y
 fY = function(D,N, Z,X){
   Byz = -1*(rnorm(D))
-  Byx = 20*(rgamma(1,3,3))
-  Y = abs((matrix(Z,ncol=D) %*% matrix(Byz,ncol=1))  +  
-            (abs((matrix(Z,ncol=D) %*% matrix(Byz,ncol=1)))) + 
-            1*Byx*X + 1*Byx*X) + 2*rnorm(N)
+  Byx = 1*(rgamma(1,3,3))
+  part1 = (matrix(Z,ncol=D) %*% matrix(Byz,ncol=1))
+  part2 = 2*(X>0.5)-1
+  Y = part1 + 3*part2*X + 2*X
+  return(Y)
 }
 
 Z = fZ(D,N)
@@ -51,4 +54,6 @@ X_intv = runif(N)
 Y_intv = fY(D,N,Z,X_intv)
 data_intv = data.frame(cbind(Y_intv,X_intv,Z))
 
-
+plot(data_intv[,2],data_intv[,1])
+plot(data_obs[,2],data_obs[,1])
+hist(X)
