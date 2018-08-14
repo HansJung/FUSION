5526dd9c521b9d7f1964c97fe714e37d82956749
Hans Jung <yhansjung@gmail.com>
2018-08-14 17:59:14 -0400

    180814 Propensity score (cont/disc are in hidden block)

--- propensity_visual.R
+++ propensity_visual.R
@@ -3,7 +3,6 @@ library(ggplot2)
 library(SuperLearner)
 require(stats); require(graphics)
 library(splines)
-library(cowplot)
 
 ################## Introduction ##################
 
@@ -33,32 +32,11 @@ dimZ = function(Z){
   return(ncol(Z))
 }
 
-addSmallNoise = function(Z){
-  eps = 1e-3
-  N = dim(Z)[1]
+addSmallNoise = function(Z, eps){
+  # Input Z  in 1D 
+  N = dim(Z)[1] # Number of samples 
   Z = Z+eps*rnorm(N)
   return(Z)
-}
-
-discCheckZ = function(Z){
-  # Check whether Z is continuous or discrete. 
-  ## If unique(Z) > 2, then Z is continuous 
-  discreteSwitch = TRUE 
-  
-  # In this function, Z is either 1D or higher. 
-  # Suppose Z is ncol(Z) dimensional. 
-  # Let Zi be i'th column of Z. 
-  for (colidx in 1:ncol(Z)){ # For each column of Z (Zi)
-    zi = Z[,colidx] # i'th dimensional 
-    if (length(unique(zi)) > 2){
-      discreteSwitch = FALSE 
-      return(FALSE)
-    }
-    else{
-      next
-    }
-  }
-  return(discreteSwitch)
 }
 
 # Training SuperLearner
@@ -74,57 +52,32 @@ trainSL = function(Y,X,family, numFold){
   return(train_model)
 }
 
-min.mean.sd.max <- function(x) {
-  # Extract min, mean-sd, mean, mean+sd, max 
-  r <- c(min(x), mean(x) - sd(x), mean(x), mean(x) + sd(x), max(x))
-  names(r) <- c("ymin", "lower", "middle", "upper", "ymax")
-  return(r)
-}
-
-# Draw a box plot (using mean and vairance) if Z is discrete. 
-drawBoxPlot = function(dfXZ, unique_Z){
-  dfXZ.X.Z1 = dfXZ[dfXZ[2]==unique_Z[1],1]
-  dfXZ.X.Z2 = dfXZ[dfXZ[2]==unique_Z[2],1]
-  
-  groupZ = rep(1:2,c(length(dfXZ.X.Z1),length(dfXZ.X.Z2)))
-  groupZ = factor(groupZ,labels=c('Z1','Z2'))
-  
-  mydata = data.frame(c(dfXZ.X.Z1,dfXZ.X.Z2), groupZ)
-  names(mydata) = c("value","group")
-  
-  fill <- "#56B4E9"
-  line <- "#1F3552"
-  
-  gg1 = ggplot(aes(y = value, x = factor(group)), data = mydata)
-  gg1 = gg1 + stat_summary(fun.data = min.mean.sd.max, geom = "boxplot", fill=fill, colour=line,
-                                 outlier.colour = "#1F3552", outlier.shape = 20)
-  return(gg1)
-}
 
 # Draw a spline plot if Z is continuous. 
-drawSpilnePlot = function(Z,px.z.prediction){
+drawSplinePlot = function(Z,px.z.prediction, n, B, alpha_confidence, mycolor){
   # Input: Z, P(X=1|Z=zi), for i=1,2,...,N
+  # - Z: Z 
+  # - px.z.prediction: estimated propensity score 
+  # - n: number of estimating points of X or Y for spline 
+  # - B: number of experiment for computing uncertainty.
+  # - alpha_confidence: confidence interval for spline CI
+  # - mycolor: plot of color 
   # Output: plot
   df1 = data.frame(Z,px.z.prediction)
   colnames(df1)[1] = 'x'
   colnames(df1)[2] = 'y'
-  
-  n = 50 
-  B = 200
-  alpha = 0.01
-  mycolor = 'red'
-  result_spline = spline_CI(df1,n,B,alpha)
+  # 
+  # n = 50 
+  # B = 200
+  # alpha = 0.01
+  # mycolor = 'red'
+  result_spline = spline_CI(df1,n,B,alpha_confidence)
   df2 = data.frame( x=result_spline$x, y=result_spline$main.curve ) 
   df3 = data.frame( x=result_spline$x, ymin=result_spline$lower.ci, ymax=result_spline$upper.ci, y=result_spline$main.curve   )
   
   gg1 = ggplot(df1, aes(x=x, y=y)) + geom_point(size=1.5, color = mycolor)
   gg1 = gg1 + geom_line(data=df2,aes(x=x, y=y), color=mycolor)
   gg1 = gg1 + geom_ribbon(data=df3, aes(x=x, ymin=ymin,ymax=ymax),alpha=0.4, fill=mycolor)
-  # gg1 = gg1 + geom_vline(data=df1,xintercept=quant_X, linetype='dotted')
-  
-  # gg = ggplot(data=df.plot,aes(z,ps)) +
-  #   geom_point(size=1.5,color='red') +
-  #   geom_line(data=data.frame(spline(df.plot,n=10)),aes(x,y), color='red')
   return(gg1)
 }
 
@@ -186,24 +139,11 @@ spline_estimator <- function(data,n) {
   return(predict(fit,x=eval_grid)$y) # We only want the predicted values
 }
 
-
-
-
-
-
-
-applyTheme = function(gg1, title,TF_discZ){
+applyTheme = function(gg1, title){
   # Apply theme
   gg1 = gg1 + ggtitle(title)
-  gg1 = gg1 + coord_cartesian(ylim=c(0,1))
   gg1 = gg1 + theme_bw()
-  if (TF_discZ == FALSE){ # continuous Z 
-    gg1 = gg1 + scale_x_continuous(name = "Z=z") + scale_y_continuous(name = "P(X=1|Z=z)")  
-  }
-  else{
-    gg1 = gg1 + scale_x_discrete(name = "Z=z") + scale_y_continuous(name = "P(X=1|Z=z)")  
-  }
-  
+  gg1 = gg1 + scale_x_continuous(name = "X") + scale_y_continuous(name = "Estimated Y",limits=c(min(Y), A=max(Y)))
   gg1 = gg1 + theme(axis.line.x = element_line(size = 0.5, colour = "black"),
                     axis.line.y = element_line(size = 0.5, colour = "black"),
                     axis.line = element_line(size=1, colour = "black"),
@@ -216,21 +156,109 @@ applyTheme = function(gg1, title,TF_discZ){
 
 
 
+
+
+
+
+
+
+# applyTheme = function(gg1, title,TF_discZ){
+#   # Apply theme
+#   gg1 = gg1 + ggtitle(title)
+#   gg1 = gg1 + coord_cartesian(ylim=c(0,1))
+#   gg1 = gg1 + theme_bw()
+#   if (TF_discZ == FALSE){ # continuous Z 
+#     gg1 = gg1 + scale_x_continuous(name = "Z=z") + scale_y_continuous(name = "P(X=1|Z=z)")  
+#   }
+#   else{
+#     gg1 = gg1 + scale_x_discrete(name = "Z=z") + scale_y_continuous(name = "P(X=1|Z=z)")  
+#   }
+#   
+#   gg1 = gg1 + theme(axis.line.x = element_line(size = 0.5, colour = "black"),
+#                     axis.line.y = element_line(size = 0.5, colour = "black"),
+#                     axis.line = element_line(size=1, colour = "black"),
+#                     panel.border = element_blank(),
+#                     panel.background = element_blank(),
+#                     plot.title=element_text(size = 20),
+#                     text=element_text(size = 16))
+#   return(gg1)
+# }
+
+
+# min.mean.sd.max <- function(x) {
+#   # Extract min, mean-sd, mean, mean+sd, max 
+#   r <- c(min(x), mean(x) - sd(x), mean(x), mean(x) + sd(x), max(x))
+#   names(r) <- c("ymin", "lower", "middle", "upper", "ymax")
+#   return(r)
+# }
+
+# discCheckZ = function(Z){
+#   # Check whether Z is continuous or discrete. 
+#   ## If unique(Z) > 2, then Z is continuous 
+#   discreteSwitch = TRUE 
+#   
+#   # In this function, Z is either 1D or higher. 
+#   # Suppose Z is ncol(Z) dimensional. 
+#   # Let Zi be i'th column of Z. 
+#   for (colidx in 1:ncol(Z)){ # For each column of Z (Zi)
+#     zi = Z[,colidx] # i'th dimensional 
+#     if (length(unique(zi)) > 2){
+#       discreteSwitch = FALSE 
+#       return(FALSE)
+#     }
+#     else{
+#       next
+#     }
+#   }
+#   return(discreteSwitch)
+# }
+
+
+# # Draw a box plot (using mean and vairance) if Z is discrete. 
+# drawBoxPlot = function(dfXZ, unique_Z){
+#   dfXZ.X.Z1 = dfXZ[dfXZ[2]==unique_Z[1],1]
+#   dfXZ.X.Z2 = dfXZ[dfXZ[2]==unique_Z[2],1]
+#   
+#   groupZ = rep(1:2,c(length(dfXZ.X.Z1),length(dfXZ.X.Z2)))
+#   groupZ = factor(groupZ,labels=c('Z1','Z2'))
+#   
+#   mydata = data.frame(c(dfXZ.X.Z1,dfXZ.X.Z2), groupZ)
+#   names(mydata) = c("value","group")
+#   
+#   fill <- "#56B4E9"
+#   line <- "#1F3552"
+#   
+#   gg1 = ggplot(aes(y = value, x = factor(group)), data = mydata)
+#   gg1 = gg1 + stat_summary(fun.data = min.mean.sd.max, geom = "boxplot", fill=fill, colour=line,
+#                                  outlier.colour = "#1F3552", outlier.shape = 20)
+#   return(gg1)
+# }
+
+
+
+
 ################## MAIN: Case 1, if Z is 1D ##################
 
+# Step 0. Configuration 
+eps = 1e-4 # constant for adding small noise 
+n = 50 # number of sampling from X. This number of points are shown in the resulting plot. 
+B = 100 # number of experiment for computing uncertainty (called bootstrap iteration)
+alpha_confidence = 0.01 # Confidence interval (100-alpha)%
+plot_color = "red"
+
 # Step 1. Load data 
-# source('datagen/data_generation_simpson_disc.R') # Z 1D, discrete
-source('datagen/data_generation_contZ_1D.R') # Z 1D, continuous
+source('datagen/data_generation_simpson_disc.R') # Z 1D, discrete
+# source('datagen/data_generation_contZ_1D.R') # Z 1D, continuous
 
 # Step 2. Extract XZ from data frame (Y,X,Z)
 XZ = extractXZ(data)
 X = XZ[[1]]
 Z = XZ[[2]]
-Z = addSmallNoise(Z)
-dfXZ = data.frame(X,Z) 
-
+## Add small noises to Z for making Z continuous. 
+Z = addSmallNoise(Z,eps)
 dimZ(Z) # Check that dimension of Z (in this example, dim(Z) must be 1)
-discCheckZ(Z) # Check whether Z is discrete or Not.
+## Wrap up (X,Z)
+dfXZ = data.frame(X,Z) 
 
 # Computing propensity score
 px.z = trainSL(X,Z,'disc',5)
@@ -238,8 +266,8 @@ px.z = trainSL(X,Z,'disc',5)
 px.z.prediction = predict(px.z,Z,onlySL = T)$pred
 
 # Draw a plot 
-g_cont = drawSpilnePlot(Z,px.z.prediction) # Only a middle spline line without uncertainty shaded area. 
-# g_cont = applyTheme(g_cont,'Propensity score',discCheckZ(Z))
+g_cont = drawSplinePlot(Z,px.z.prediction, n, B, alpha_confidence, plot_color) # Only a middle spline line without uncertainty shaded area. 
+g_cont = applyTheme(g_cont,'Propensity score')
 g_cont
 
 
